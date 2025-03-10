b9ac1233196dca61f76504a95a03710a9e17fd99
dfgdgdge <gelenadom@gmail.com>
2025-03-10 23:46:24 +0300

    draft start/help

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('bot')
+import telebot
+
+bot = telebot.TeleBot('8086994241:AAG8NYaP-2dxDJMyKFnqutIMCs-nUIxaLys')
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Привет! (здесь будет инфа о боте)")
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.send_message(message.chat.id, "(здесь будет руководство по боту)")
+
+
+bot.polling(none_stop=True)
